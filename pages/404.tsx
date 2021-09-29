4cc1450ec208be486a968e5bc86e86964f46d9e3
asoyuta <asoyuta@icloud.com>
2021-09-30 01:58:39 +0900

    commit

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,12 @@ import { useRouter } from 'next/router'
 
 const NotFound = () => {
   const router = useRouter()
-
+  
   useEffect(() => {
     setTimeout(() => {
       router.push('/')
     }, 3000)
-  }, [])
+  }, [router])
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
